2176454df8180b97cdf7235cd92684af9d45286d
Dreamboater <adrian.t.vandervaart@gmail.com>
2024-02-23 00:30:19 -0500

    Can play cards Checks if card is playable checks if you can't play, auto draws, auto plays if you can

--- main.py
+++ main.py
@@ -1,50 +1,108 @@
 # UNO
 import random
+
+
 class Card():
-    def __init__(self,color:str,type:str,value:int):
+    def __init__(self, color: str, type: str, value: int):
         self.color = color
         self.type = type
         self.value = value
 
-players = []
-hand = []
-deck = []
 
-type = ["number", "+2","+4 wild","skip","reverse","wild"]
+players = []
+deck = []
+discard = []
+
+type = ["number", "+2", "+4 wild", "skip", "reverse", "wild"]
 # 0, 1-9x2, 8 skip reverse +2, 4 wild +4wild
-colors = ["Blue","Green","Red","Yellow"]
+colors = ["Blue", "Green", "Red", "Yellow"]
 
 for t in type:
     if t == "number":
         for color in colors:
-            deck.append(Card(color,"0",0))
+            deck.append(Card(color, "0", 0))
         for j in range(2):
             for color in colors:
-                for i in range(1,10):
-                 deck.append(Card(color,int(i),i))
-    if t == "+2" or t == "skip" or t == "reverse":
+                for i in range(1, 10):
+                    deck.append(Card(color, i, i))
+    if t == "+2" or t == "Skip" or t == "Reverse":
         for color in colors:
             for i in range(2):
-                deck.append(Card(color,t,20))
-    if t == "+4 wild" or t== "wild":
+                deck.append(Card(color, t, 20))
+    if t == "+4 Wild" or "Wild" == t:
         for i in range(4):
-            deck.append(Card("",t,50))
+            deck.append(Card("All", t, 50))
 
 
-def print_deck(deck: Card):
-    for item in deck:
-        print(item.color, item.type, end =",")
+def print_deck(dek: list):
+    for item in dek:
+        print(item.color, item.type, end=",")
     print()
 
-print_deck(deck)
-print(len(deck))
 
-def create_Players():
+def allow(d: Card,d2: Card):
+    if d.color == d2.color or d.type == d2.type:
+        return True
+    return False
 
-    num = int(input("How many players are playing?"))
 
-    random.shuffle(deck)
-    print(deck)
+def possible(d: Card, hand: list):
+    for card in hand:
+        if d.color == card.color or d.type == card.type:
+            return True
+    return False
 
-    for x in range(num):
-        hand =[]
+
+
+
+num_of_players = int(input("How many players are playing?"))
+
+random.shuffle(deck)
+
+for x in range(num_of_players):
+    handy = []
+    for y in range(3):
+        handy.append(deck.pop())
+    players.append(handy)
+
+
+turn_number = 0
+
+discard.append(deck.pop())
+
+#do you check if the player can play a card for them, if not, then draw a card and check if that can be played, and
+# if it can then just play it and move on
+
+while True:
+    player_turn = turn_number%num_of_players
+    print(discard[-1].color,discard[-1].type)
+    print("It's "+str(player_turn)+" turn, hand: ", end =" ")
+    print_deck(players[player_turn])
+
+    if not possible(discard[-1],players[player_turn] ):
+        x = deck.pop()
+        if discard[-1].color == x.color or discard[-1].type == x.type:
+            discard.append(x)
+
+        else:
+            players[player_turn].append(x)
+
+        turn_number = turn_number + 1
+        continue
+
+    choice = int(input("Please player "+str(turn_number%num_of_players)+" pick a card to play on the discard pile:"))
+    if not allow(discard[-1], players[player_turn][choice]):
+        continue
+    discard.append(players[player_turn].pop(choice))
+    if len(players[player_turn]) == 0:
+        break
+    print(discard[-1].color, discard[-1].type)
+
+    print_deck(players[player_turn])
+    turn_number = turn_number + 1
+    for i in range(10):
+        print()
+
+
+
+
